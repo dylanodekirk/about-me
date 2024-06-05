070424843e41a8f5d4882f6fba25122683ae8ad1
dylanodekirk <31811209+dylanodekirk@users.noreply.github.com>
2024-06-05 12:36:42 -0500

    Added tailwind to portfolio

--- app/util.js
+++ app/util.js
@@ -9,20 +9,22 @@ import { MdHtml } from "react-icons/md";
 import { FaGithub } from "react-icons/fa";
 import { MdCss } from "react-icons/md";
 import { MdJavascript } from "react-icons/md";
+import { RiTailwindCssFill } from "react-icons/ri";
 
 export const skills = [
   { id: 1, icon: <MdHtml size="3rem" /> },
   { id: 18, icon: <MdCss size="3rem" /> },
   { id: 4, icon: <MdJavascript size="3rem" /> },
-  { id: 3, name: "Kotlin", icon: <SiKotlin size="2rem" /> },
+  { id: 13, icon: <SiMysql size="3rem" /> },
   { id: 5, name: "React", icon: <FaReact size="2rem" /> },
   {
     id: 10,
     name: "Apollo Client/Server",
     icon: <SiApollographql size="2rem" />,
   },
+  { id: 3, name: "Kotlin", icon: <SiKotlin size="2rem" /> },
   { id: 11, name: "GraphQL", icon: <GrGraphQl size="2rem" /> },
-  { id: 13, icon: <SiMysql size="3rem" /> },
+  { id: 3, name: "Kotlin", icon: <RiTailwindCssFill size="2rem" /> },
   { id: 19, name: "Node.js", icon: <FaNodeJs size="2rem" /> },
   { id: 20, name: "MongoDB", icon: <SiMongodb size="2rem" /> },
   { id: 23, name: "GitHub", icon: <FaGithub size="2rem" /> },
