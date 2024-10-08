a40331640b7c571c7d2ea2515e3213da9a51935b
dylanodekirk <31811209+dylanodekirk@users.noreply.github.com>
2024-10-08 12:24:14 -0500

    Fix the width of the tech experience section

--- app/experience.tsx
+++ app/experience.tsx
@@ -23,48 +23,36 @@ export default function Experience() {
     <div id="experience">
       {/* Technical Experience Section */}
       <div className="flex flex-col items-center justify-center w-full pb-8 bg-[#0a1128]">
-        <motion.div
-          ref={ref}
-          initial="hidden"
-          animate={controls}
-          variants={{
-            visible: { opacity: 1, y: 0 },
-            hidden: { opacity: 0, y: 0 },
-          }}
-          transition={{ duration: 0.5 }}
-          className="w-2/3 mx-auto"
+        <Typography
+          align="center"
+          className="font-sans py-8 text-[#FAFAFA]"
+          variant="h3"
         >
-          <Typography
-            align="center"
-            className="font-sans py-8 text-[#FAFAFA]"
-            variant="h3"
-          >
-            Technical Experience
-          </Typography>
+          Technical Experience
+        </Typography>
 
-          <div className="flex flex-col w-full items-center justify-center space-y-4">
-            {technicalExperience.map((job, key) => (
-              <motion.div
-                key={key}
-                style={{ backgroundColor: "#293252" }}
-                initial={{ opacity: 0, y: 10 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.3 }}
-              >
-                <ExperienceCard
-                  company={job.company}
-                  position={job.position}
-                  date={job.dates}
-                  location={job.location}
-                  jobDetails={job.jobDetails}
-                  companyLogo={job.companyLogo}
-                  bgColor={"#293252"}
-                  textColor={"#FAFAFA"}
-                />
-              </motion.div>
-            ))}
-          </div>
-        </motion.div>
+        <div className="flex flex-col w-full items-center justify-center space-y-4">
+          {technicalExperience.map((job, key) => (
+            <motion.div
+              key={key}
+              initial={{ opacity: 0, y: 10 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.3 }}
+              className="w-2/3 mx-auto"
+            >
+              <ExperienceCard
+                company={job.company}
+                position={job.position}
+                date={job.dates}
+                location={job.location}
+                jobDetails={job.jobDetails}
+                companyLogo={job.companyLogo}
+                bgColor={"#293252"}
+                textColor={"#FAFAFA"}
+              />
+            </motion.div>
+          ))}
+        </div>
       </div>
 
       {/* Gaming & Event Experience Section */}
